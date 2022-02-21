cf730ac1bbde82080389987bae552457f5ffa8df
John Arango <johnj42@gmail.com>
2022-02-21 12:15:49 -0500

    HW Done

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,30 +4,49 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+ 
+    return nombre[0].toUpperCase() + nombre.slice(1)
+
 }
+
+
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
+
 }
+
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1,n2)
 }
+
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+var sum = numeros.reduce(function(acc, valor) {
+return acc + valor
+})
+cb(sum)
 }
+
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function (elemento) {
+    cb(elemento)
+  })
 }
 
 function map(array, cb) {
@@ -35,13 +54,22 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var array2 = array.map(cb)
+  return array2
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var array2 = []
+  array.forEach(function (elemento) {
+   if (elemento[0] === "a")
+    array2.push (elemento)
+  })
+  return array2
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
